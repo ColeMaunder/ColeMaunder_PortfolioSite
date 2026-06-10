4192d9719cda42e94c7d5a5dcc45a2d708b67781
ColeMaunder <Cole@maunder.co.za>
2026-06-11 00:08:38 +0200

    Data afn finshing touches

--- JS/data.js
+++ JS/data.js
@@ -1,13 +1,13 @@
 const projects = [
-    { id: 1, name: "BloodyHunters", imageName: "hades-2.jpg", link: "../html/projects.html", alt: "image of my bloodyHunters project", explain: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." , sightLink: "https://itch.io/"},
-    { id: 2, name: "WiahfalAcent", imageName: "hades-2.jpg", link: "../html/projects.html", alt: "image of my WiahfalAcent project", explain: "stuff", sightLink: "https://itch.io/"}
+    { id: 1, name: "BloodyHunters", imageName: "Blood.png", link: "../html/projects.html", alt: "image of my bloodyHunters project", explain: "I created a turn based jrpg inspred gaem with a focaus on machanical depth." , sightLink: "https://itch.io/"},
+    { id: 2, name: "WiahfallAcent", imageName: "Wish.png", link: "../html/projects.html", alt: "image of my WiahfalAcent project", explain: "I woked in agupe to create a 3D puzzle game phsics based pusil game", sightLink: "https://itch.io/"}
 ];
 const works = [
     {
-        id: 1, name: "Rabit", imageName: "hades-2.jpg", link: "../html/works.html", alt: "image of my bloodyHunters project",
+        id: 1, name: "Rabit", imageName: "Rabit.jpeg", link: "../html/works.html", alt: "image of my bloodyHunters project",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut amnderit in.",
-        explain: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        explain: "I was inspired by gretete design to create my own tag."
     },
-    { id: 2, name: "Thesious", imageName: "hades-2.jpg", link: "../html/projects.html", alt: "image of my WiahfalAcent project", description: "why Stuff", explain: "stuff"}
+    { id: 2, name: "Thesious", imageName: "Thesious.png", link: "../html/projects.html", alt: "image of my WiahfalAcent project", description: "why Stuff", explain: "I was inpired by the idea of human Ai interaction so i creted a digital cahricter that culed act as afce to talk to an AI model"}
 ];
 
